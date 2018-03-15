25dd303611a56534202b41e9470e8c7cff3b44c6
NicklasRohman <NicklasRohman@gmail.com>
2018-03-15 08:47:27 +0100

    first commit

---
+++ src/application/AppStart.java
@@ -0,0 +1,33 @@
+package application;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.layout.BorderPane;
+import javafx.stage.Stage;
+
+public class AppStart extends Application{
+
+	public static void main(String[] args) {
+		launch(args);
+
+	}
+
+	@Override
+	public void start(Stage stage) throws Exception {
+		try {
+			BorderPane root = (BorderPane)FXMLLoader.load(getClass().getResource("Sample.fxml"));
+			Scene scene = new Scene(root,1350,720);
+
+	        stage.setTitle("FaceRecognizerOpenCVJava");
+			
+			stage.setScene(scene);
+			stage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+		
+	}
+
+}
